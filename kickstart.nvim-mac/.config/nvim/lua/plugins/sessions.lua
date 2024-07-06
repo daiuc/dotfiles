e2726d5ab2c5a358ec8c02aad9b5fa5388675b61
daiuc <chaodai@uchicago.edu>
2024-07-06 12:41:18 -0500

    added autossessions with keymap

---
+++ kickstart.nvim-mac/.config/nvim/lua/plugins/sessions.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'rmagatti/auto-session',
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+    },
+    config = function()
+      require('auto-session').setup {
+        auto_session_suppress_dirs = {
+          '~/',
+          '~/Projects',
+          '~/Downloads',
+          '~/Desktop',
+          '/',
+        },
+      }
+    end,
+  },
+}
