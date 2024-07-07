4f48118b808e5ad30807d9302ce34a9515011b75
daiuc <chaodai@uchicago.edu>
2024-07-06 23:18:52 -0500

    change autoformat

--- kickstart.nvim/.config/nvim/lua/plugins/autoformat.lua
+++ kickstart.nvim/.config/nvim/lua/plugins/autoformat.lua
@@ -12,27 +12,36 @@ return {
         desc = '[F]orma[t] buffer',
       },
     },
-    opts = {
-      notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
-        -- have a well standardized coding style. You can add additional
-        -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
-        return {
+    enabled = true,
+    config = function()
+      require('conform').setup {
+        notify_on_error = true,
+        format_on_save = {
           timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-        }
-      end,
-      formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        python = { 'isort', 'black' },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
-      },
-    },
+          lsp_fallback = true,
+        },
+        formatters_by_ft = {
+          lua = { 'stylua' },
+          python = { 'isort', 'black' },
+          quarto = { 'injected' },
+        },
+      }
+      -- Customize injected formatters
+      -- see https://github.com/jmbuhr/quarto-nvim-kickstarter/blob/382b050e13eada7180ad048842386be37e820660/lua/plugins/editing.lua#L29-L81
+      require('conform').formatters.injected = {
+        options = {
+          ignore_errors = true,
+          lang_to_ext = {
+            bash = 'sh',
+            r = 'r',
+            python = 'py',
+            markdown = 'md',
+          },
+          -- Map of treesitter language to formatters to use
+          -- (defaults to the value from formatters_by_ft)
+          lang_to_formatters = {},
+        },
+      }
+    end,
   },
 }
