69499d5b7e03b2bbb95e8cc23fc6c3ff08fe8d40
daiuc <chaodai@uchicago.edu>
2024-07-07 19:53:02 -0500

    remove snakemake from pyright lsp

--- kickstart.nvim/.config/nvim/lua/plugins/lsp.lua
+++ kickstart.nvim/.config/nvim/lua/plugins/lsp.lua
@@ -159,7 +159,7 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         pyright = {
-          filetypes = { 'python', 'snakemake' },
+          filetypes = { 'python' },
           flags = lsp_flags,
           -- settings = {
           --   python = {
