03af759d9f5f8f7d513c6d847543683ac466c5bf
daiuc <chaodai@uchicago.edu>
2024-07-09 17:28:29 -0500

    allow vscode nvim extension

--- kickstart.nvim/.config/nvim/init.lua
+++ kickstart.nvim/.config/nvim/init.lua
@@ -1,3 +1,9 @@
-require 'config.global'
-require 'config.lazy'
-require 'config.autocommands'
+if vim.g.vscode then
+  require 'config-vscode.global'
+  require 'config-vscode.lazy'
+  require 'config-vscode.autocommands'
+else
+  require 'config.global'
+  require 'config.lazy'
+  require 'config.autocommands'
+end
