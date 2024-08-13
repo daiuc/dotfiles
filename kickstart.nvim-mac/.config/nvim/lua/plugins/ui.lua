0189418361576cea306768622620d2e7ea47720c
daiuc <chaodai@uchicago.edu>
2024-08-12 20:24:19 -0500

    added noice nvim plugin

--- kickstart.nvim-mac/.config/nvim/lua/plugins/ui.lua
+++ kickstart.nvim-mac/.config/nvim/lua/plugins/ui.lua
@@ -74,6 +74,50 @@ return {
       },
     },
   },
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {},
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      { 'MunifTanjim/nui.nvim', module = 'nui' },
+      { 'rcarriga/nvim-notify', module = 'notify' },
+    },
+    config = function()
+      require('noice').setup {
+        background_colour = '#ace1af',
+        cmdline = {
+          enabled = true,
+          view = 'cmdline_popup',
+        },
+        messages = {
+          enabled = true,
+        },
+        popupmenu = {
+          enabled = true,
+        },
+        notify = {
+          enabled = true,
+        },
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+            ['vim.lsp.util.stylize_markdown'] = true,
+            ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = false, -- use a classic bottom cmdline for search
+          command_palette = false, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = true, -- add a border to hover docs and signature help
+        },
+      }
+    end,
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
