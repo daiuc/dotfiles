3b5707999c4ba650becec0af4308cd24b1f3d103
daiuc <chaodai@uchicago.edu>
2024-09-13 18:59:06 -0500

    add neogit and other git extensions for kickstart on mac

--- kickstart.nvim-mac/.config/nvim/lua/plugins/git.lua
+++ kickstart.nvim-mac/.config/nvim/lua/plugins/git.lua
@@ -11,4 +11,51 @@ return {
       },
     },
   },
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+      'nvim-telescope/telescope.nvim', -- optional
+    },
+    lazy = true,
+    keys = {
+      { '<leader>gg', ':Neogit<cr>', desc = 'neo[g]it' },
+    },
+    config = function()
+      require('neogit').setup {
+        disable_commit_confirmation = false,
+        integrations = {
+          diffview = true,
+        },
+      }
+    end,
+  },
+  {
+    'akinsho/git-conflict.nvim',
+    init = function()
+      require('git-conflict').setup {
+        default_mappings = false,
+        disable_diagnostics = true,
+      }
+    end,
+    keys = {
+      { '<leader>gco', ':GitConflictChooseOurs<cr>' },
+      { '<leader>gct', ':GitConflictChooseTheirs<cr>' },
+      { '<leader>gcb', ':GitConflictChooseBoth<cr>' },
+      { '<leader>gc0', ':GitConflictChooseNone<cr>' },
+      { ']x', ':GitConflictNextConflict<cr>' },
+      { '[x', ':GitConflictPrevConflict<cr>' },
+    },
+    {
+      'f-person/git-blame.nvim',
+      init = function()
+        require('gitblame').setup {
+          enabled = false,
+        }
+        vim.g.gitblame_display_virtual_text = 1
+        -- vim.g.gitblame_enabled = 0
+      end,
+    },
+  },
 }
