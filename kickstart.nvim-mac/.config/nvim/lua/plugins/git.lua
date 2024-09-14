b2433bea535732a81d529b6dbc54aec922fc6f59
daiuc <chaodai@uchicago.edu>
2024-09-13 22:37:02 -0500

    set up lazygit nvim-mac

--- kickstart.nvim-mac/.config/nvim/lua/plugins/git.lua
+++ kickstart.nvim-mac/.config/nvim/lua/plugins/git.lua
@@ -12,7 +12,27 @@ return {
     },
   },
   {
+    'kdheepak/lazygit.nvim',
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>gg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+  },
+  {
     'NeogitOrg/neogit',
+    enabled = false,
     dependencies = {
       'nvim-lua/plenary.nvim', -- required
       'sindrets/diffview.nvim', -- optional - Diff integration
@@ -33,6 +53,7 @@ return {
   },
   {
     'akinsho/git-conflict.nvim',
+    enabled = false,
     init = function()
       require('git-conflict').setup {
         default_mappings = false,
