067b19523a03dc1000c2b1d9478d130386a5a2eb
Chao Dai <chaodai@uchicago.edu>
2024-09-15 15:26:03 -0500

    wezterm transparency

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -80,7 +80,7 @@ config.keys = {
 --windows
 config.window_decorations = "RESIZE"
 config.window_background_opacity = 0.8
-config.macos_window_background_blur = 30
+config.macos_window_background_blur = 10
 
 config.hyperlink_rules = {
 	-- Matches: a URL in parens: (URL)
